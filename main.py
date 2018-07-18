c2d2abeb5e4850d1b1e9a1e36cfaae741923e742
jorgee1 <jorgepacheco@uninorte.edu.co>
2018-07-18 17:20:27 -0500

    Falta corregir lo del indice loco que no se resetea cuandola ventanna no tiene focus

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import csv
 import ntpath
 from PyQt5.QtWidgets import QMainWindow, QApplication, QInputDialog, QLineEdit, QFileDialog
+from PyQt5.QtCore import (QCoreApplication, Qt, QEvent)
 from mainUI import Ui_MainWindow
 
 """
@@ -15,25 +16,48 @@ Jorge Ortega
 class AppWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.listIndex = -1
+
+
+
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.open_fileB.triggered.connect(self.openFileNameDialog)
-        self.ui.pushButton.clicked.connect(self.remove_item)
+        self.ui.pushButton.clicked.connect(self.extract_data)
+        self.ui.listWidget.itemSelectionChanged.connect(self.test)
+
+    def test(self):
+        print(self.ui.listWidget.selectedItem().text())
+
+    def keyPressEvent(self, event):
+        key = event.key()
+
+        if key == Qt.Key_Escape:
+            QCoreApplication.quit()
+        elif key == Qt.Key_Delete:
+            print(self.ui.listWidget.currentRow())
+            if self.ui.listWidget.currentRow()>=0:
+                item = self.ui.listWidget.takeItem(self.ui.listWidget.currentRow())
+                item = None
+
+    def mousePressEvent(self, event):
+        #print("X:",event.x(),", Y:",event.y())
+        for i in range(self.ui.listWidget.count()):
+            self.ui.listWidget.item(i).setSelected(False)
+        self.ui.pushButton.setFocus()
 
     def openFileNameDialog(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileNames(self,"Abrir CSV", "","CSV (*.csv)", options=options)
-        if fileName:
-            self.ui.listWidget.addItem(ntpath.basename(fileName))
-            print(self.ui.listWidget.count())
-            print(self.ui.listWidget.item(0))
+        fileNames, _ = QFileDialog.getOpenFileNames(self,"Abrir TXT", "","TXT (*.txt)", options=options)
+        if fileNames:
+            self.ui.listWidget.addItems([ntpath.basename(i) for i in fileNames])
+        self.ui.pushButton.setFocus()
 
-    def remove_item(self):
-        print(self.ui.listWidget.currentRow())
-        if self.ui.listWidget.currentRow()>=0:
-            item = self.ui.listWidget.takeItem(self.ui.listWidget.currentRow())
-            item = None
+    def extract_data(self):
+        for i in range(self.ui.listWidget.count()):
+            print(self.ui.listWidget.item(i).text())
+
 
 app = QApplication(sys.argv)
 w = AppWindow()
